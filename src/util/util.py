72ecfc03fb6d99f0675aff56dae9805925091e11
Smyran Math <smyranmath@gmail.com>
2026-06-02 11:21:38 -0400

    Added function to get video properties

--- src/util/util.py
+++ src/util/util.py
@@ -8,5 +8,20 @@ def load_video(path="resources/video.mp4") -> cv2.VideoCapture:
         return None
     return cap
 
+# Returns the properties of the video as a dictionary
+def get_video_properties(vid: cv2.VideoCapture):
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    properties = {
+        "width": width,
+        "height": height,
+        "fps": fps,
+        "frames": frames
+    }
+    return properties
+
 if __name__ == "__main__":
     print("Running utility functions...")
